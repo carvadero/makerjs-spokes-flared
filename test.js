93d8fc2f61442ee046b71e9ff68300ba240f6ab9
Dan Marshall <danmar@microsoft.com>
2015-12-27 11:53:06 -0800

    updated wedge, added browserify

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 var makerjs = require('makerjs');
 var StraightSpokes = require('./index.js');
-console.log(makerjs.exporter.toSVG(new StraightSpokes(100, 30, 10, 5, 0, false)));
+console.log(makerjs.exporter.toSVG(new StraightSpokes(100, 30, 10, 5, 0, .1, .1, false)));
