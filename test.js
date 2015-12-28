9aabbcd6f9b1a0603e847f310f97e18c665b2dc9
carvadero <carvadero@gmail.com>
2015-12-28 15:54:36 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 var makerjs = require('makerjs');
-var StraightSpokes = require('./index.js');
-console.log(makerjs.exporter.toSVG(new StraightSpokes(100, 30, 10, 5, 0, .1, .1, false)));
+var FlaredSpokes = require('./index.js');
+console.log(makerjs.exporter.toSVG(new FlaredSpokes(100, 30, 10, 5, 2, .1, .1, false)));
